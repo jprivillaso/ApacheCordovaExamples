b54d21bf3b6cd97882f99e8750ed3a3870ab52d7
Juan Rivillas <juan@viridis-solutions.com>
2014-09-27 11:05:14 -0500

    Remove Unnecessary files

--- hello/platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Juan Rivillas\workspace\ANDROID\ANDROID-DEV\hello\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\Juan Rivillas\workspace\ANDROID\ANDROID-DEV\hello\platforms\android\AndroidManifest.xml \
